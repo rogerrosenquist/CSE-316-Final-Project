376641049fd0aa13e1c9d19237aae82376e28923
Zhen Yi Pan <zharnite@gmail.com>
2020-11-19 10:38:19 -0500

    create navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,40 @@
+import { Route, Switch } from "react-router-dom";
+
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import AppNavbar from "./components/AppNavbar";
 
 function App() {
   return (
     <div className="App">
+      <AppNavbar />
       <h1>hello</h1>
+
+      <Switch>
+        <Route
+          path={["/", "/labtech"]}
+          render={() => {
+            <div>
+              <h1>Labtech page placeholder</h1>
+              <h2>Placeholder for a react component</h2>
+              <p>Enclose this section with '()' for a react component</p>
+            </div>;
+          }}
+          exact
+        />
+        <Route
+          path="/employee"
+          render={() => {
+            <div>
+              <h1>Employee page placeholder</h1>
+              <h2>Placeholder for a react component</h2>
+              <p>Enclose this section with '()' for a react component</p>
+            </div>;
+          }}
+          exact
+        />
+      </Switch>
     </div>
   );
 }
---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  Nav,
+  NavItem,
+  NavLink,
+  Container,
+  NavbarBrand,
+} from "reactstrap";
+import { Link } from "react-router-dom";
+
+const AppNavbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="dark" dark expand="sm" className="mb-5">
+        <Container>
+          <NavbarBrand tag={Link} to="/">
+            SBU COVID Testing Interface
+          </NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                <NavLink tag={Link} to="/labtech">
+                  LabTech
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Link} to="/employee">
+                  Employee
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default AppNavbar;
