a4981a576694e0064b8744c2df63d52768f63cf2
Zhen Yi Pan <zharnite@gmail.com>
2020-11-19 19:46:11 -0500

    testing types and testing reducer

---
+++ client/src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import itemReducer from "./itemReducer"; // TESTING PURPOSES
+
+export default combineReducers({
+  item: itemReducer, // TESTING PURPOSES
+});
---
+++ client/src/reducers/itemReducer.js
@@ -0,0 +1,14 @@
+/**
+ * THIS FILE IS FOR TESTING PURPOSES ONLY
+ */
+
+import { v4 as uuid } from "uuid";
+
+const initialState = {
+  items: [
+    { id: uuid(), name: "amethyst" },
+    { id: uuid(), name: "jasper" },
+    { id: uuid(), name: "agate" },
+    { id: uuid(), name: "chalcedony" },
+  ],
+};
