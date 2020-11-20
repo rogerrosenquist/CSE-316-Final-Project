f039f19cf9c92ea8dea68577f5c392631e88b26f
Zhen Yi Pan <zharnite@gmail.com>
2020-11-20 01:48:46 -0500

    revert back to working employeelist

--- client/src/components/EmployeeList.js
+++ client/src/components/EmployeeList.js
@@ -6,7 +6,7 @@
  * The backend is not hooked up, so adding/deleting is ok as of now.
  */
 
-import React, { Component, useEffect } from "react";
+import React, { Component } from "react";
 import { Container, ListGroup, ListGroupItem, Button } from "reactstrap";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import { v4 as uuid } from "uuid";
@@ -14,46 +14,75 @@ import { connect } from "react-redux";
 import { getEmployees, deleteEmployee } from "../actions/employeeActions";
 import PropTypes from "prop-types";
 
-const EmployeeList = (props) => {
-  useEffect(() => {
-    props.getEmployees();
-  });
+// const EmployeeList = () => {
+class EmployeeList extends Component {
+  componentDidMount() {
+    this.props.getEmployees();
+  }
 
-  const { employees } = this.props.employee;
-  return (
-    <Container>
-      <ListGroup>
-        <TransitionGroup className="employee-list">
-          {employees.map(
-            ({
-              isLabWorker,
-              _id,
-              employeeID,
-              email,
-              firstName,
-              lastName,
-              passcode,
-            }) => (
-              <CSSTransition key={_id} timeout={500} classNames="fade">
-                <ListGroupItem>
-                  <Button
-                    className="remove-btn"
-                    color="danger"
-                    size="sm"
-                    // onClick={}
-                  >
-                    &times;
-                  </Button>
-                  {firstName} {lastName}
-                </ListGroupItem>
-              </CSSTransition>
-            )
-          )}
-        </TransitionGroup>
-      </ListGroup>
-    </Container>
-  );
-};
+  render() {
+    const { employees } = this.props.employee;
+    return (
+      <Container>
+        <ListGroup>
+          <TransitionGroup className="employee-list">
+            {employees.map(
+              ({
+                isLabWorker,
+                _id,
+                employeeID,
+                email,
+                firstName,
+                lastName,
+                passcode,
+              }) => (
+                <CSSTransition key={_id} timeout={500} classNames="fade">
+                  <ListGroupItem>
+                    <Button
+                      className="remove-btn"
+                      color="danger"
+                      size="sm"
+                      // onClick={}
+                    >
+                      &times;
+                    </Button>
+                    {firstName} {lastName}
+                  </ListGroupItem>
+                </CSSTransition>
+              )
+            )}
+          </TransitionGroup>
+        </ListGroup>
+      </Container>
+    );
+  }
+
+  // const { employees } = this.props.employee;
+
+  // return (
+  //   <Container>
+  //     <ListGroup>
+  //       <TransitionGroup className="employee-list">
+  //         {employees.map((employee) => (
+  //           <CSSTransition key={employee._id} timeout={500} classNames="fade">
+  //             <ListGroupItem>
+  //               <Button
+  //                 className="remove-btn"
+  //                 color="danger"
+  //                 size="sm"
+  //                 onClick={}
+  //               >
+  //                 &times;
+  //               </Button>
+  //               {employee}
+  //             </ListGroupItem>
+  //           </CSSTransition>
+  //         ))}
+  //       </TransitionGroup>
+  //     </ListGroup>
+  //   </Container>
+  // );
+}
 
 EmployeeList.propTypes = {
   getEmployees: PropTypes.func.isRequired,
