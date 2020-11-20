cf324f05a82de6e397cde48453891e45ca743ab4
Zhen Yi Pan <zharnite@gmail.com>
2020-11-19 19:36:34 -0500

    created store and added provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,60 @@
 import { Route, Switch } from "react-router-dom";
 
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-
 import ItemList from "./components/ItemList";
 import AppNavbar from "./components/AppNavbar";
 
+import { Provider } from "react-redux";
+import store from "./store";
+
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
   return (
-    <div className="App">
-      <AppNavbar />
+    <Provider store={store}>
+      <div className="App">
+        <AppNavbar />
 
-      <Switch>
-        <Route
-          path={["/"]}
-          render={() => (
-            <div>
-              <h4>Home page placeholder for component</h4>
-            </div>
-          )}
-          exact
-        />
-        <Route
-          path={["/labtech"]}
-          render={() => (
-            <div>
-              <h4>Labtech page placeholder for a react component</h4>
-            </div>
-          )}
-          exact
-        />
-        <Route
-          path="/employee"
-          render={() => (
-            <div>
-              <h4>Employee page placeholder for a react component</h4>
-            </div>
-          )}
-          exact
-        />
-        <Route path={["/test", "/items"]} render={() => <ItemList />} exact />
-        <Route
-          path="/"
-          render={() => (
-            <div>
-              <h4>404 Page not found component placeholder</h4>
-            </div>
-          )}
-        />
-      </Switch>
-    </div>
+        <Switch>
+          <Route
+            path={["/"]}
+            render={() => (
+              <div>
+                <h4>Home page placeholder for component</h4>
+              </div>
+            )}
+            exact
+          />
+          <Route
+            path={["/labtech"]}
+            render={() => (
+              <div>
+                <h4>Labtech page placeholder for a react component</h4>
+              </div>
+            )}
+            exact
+          />
+          <Route
+            path="/employee"
+            render={() => (
+              <div>
+                <h4>Employee page placeholder for a react component</h4>
+              </div>
+            )}
+            exact
+          />
+          <Route path={["/test", "/items"]} render={() => <ItemList />} exact />
+          <Route
+            path="/"
+            render={() => (
+              <div>
+                <h4>404 Page not found component placeholder</h4>
+              </div>
+            )}
+          />
+        </Switch>
+      </div>
+    </Provider>
   );
 }
 
