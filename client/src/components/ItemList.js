e24a076a1e3a03cb264759c545bfa8d636b43285
Zhen Yi Pan <zharnite@gmail.com>
2020-11-20 01:16:17 -0500

    updated proptypes

--- client/src/components/ItemList.js
+++ client/src/components/ItemList.js
@@ -48,6 +48,7 @@ class ItemList extends Component {
 
 ItemList.propTypes = {
   getItems: PropTypes.func.isRequired,
+  deleteItem: PropTypes.func.isRequired,
   item: PropTypes.object.isRequired,
 };
 
