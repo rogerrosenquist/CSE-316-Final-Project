4ec2e6426dcf7c54d5e5ca8aa759a0626dc9357e
Zhen Yi Pan <zharnite@gmail.com>
2020-11-23 09:41:36 -0500

    pool actions

---
+++ client/src/actions/poolActions.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+import * as actions from "./types";
+
+export const getPools = () => (dispatch) => {
+  dispatch(setPoolsLoading());
+  axios.get("/api/pools").then((res) =>
+    dispatch({
+      type: actions.GET_POOLS,
+      payload: {
+        pools: res.data,
+      },
+    })
+  );
+};
+
+export const addPool = (pool) => (dispatch) => {
+  axios.post("/api/pools", pool).then((res) =>
+    dispatch({
+      type: actions.ADD_POOL,
+      payload: {
+        pool: res.data,
+      },
+    })
+  );
+};
+
+export const updatePool = (pool) => (dispatch) => {
+  axios.put(`/api/pools/${pool._id}`, pool).then((res) => {
+    dispatch({
+      type: actions.UPDATE_POOL,
+      payload: {
+        pool: pool,
+      },
+    });
+  });
+};
+
+export const deletePool = (id) => (dispatch) => {
+  axios.delete(`/api/pool/${id}`).then((res) =>
+    dispatch({
+      type: actions.DELETE_POOL,
+      payload: {
+        id: id,
+      },
+    })
+  );
+};
+
+export const setPoolsLoading = () => {
+  return {
+    type: actions.POOLS_LOADING,
+  };
+};
