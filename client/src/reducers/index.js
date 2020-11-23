2ccfe715d0aa2674b793cc089ce7a736406bc1b9
Zhen Yi Pan <zharnite@gmail.com>
2020-11-23 08:34:07 -0500

    created additional reducers and removed testing comments for relevant files

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,18 @@
 import { combineReducers } from "redux";
 import itemReducer from "./itemReducer"; // TESTING PURPOSES
-import employeeReducer from "./employeeReducer"; // TESTING PURPOSES
-import employeeTestReducer from "./employeeTestReducer"; // TESTING PURPOSES
+import employeeReducer from "./employeeReducer";
+import employeeTestReducer from "./employeeTestReducer";
+import poolMapReducer from "./poolMapReducer";
+import poolReducer from "./poolReducer";
+import wellTestingReducer from "./wellTestingReducer";
+import wellReducer from "./wellReducer";
 
 export default combineReducers({
   item: itemReducer, // TESTING PURPOSES
-  employee: employeeReducer, // TESTING PURPOSES
-  employeeTest: employeeTestReducer, // TESTING PURPOSES
+  employee: employeeReducer,
+  employeeTest: employeeTestReducer,
+  poolMap: poolMapReducer,
+  pool: poolReducer,
+  wellTesting: wellTestingReducer,
+  well: wellReducer,
 });
