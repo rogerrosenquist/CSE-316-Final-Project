78a516bc586916da2418790d36d9fc12b9c334fd
Zhen Yi Pan <zharnite@gmail.com>
2020-11-23 09:39:38 -0500

    poolReducer

--- client/src/reducers/poolReducer.js
+++ client/src/reducers/poolReducer.js
@@ -7,34 +7,30 @@ const initialState = {
 
 export default function (state = initialState, action) {
   switch (action.type) {
-    case actions.GET_EMPLOYEES:
+    case actions.GET_POOLS:
       return {
         ...state,
-        employees: action.payload.employees,
+        pools: action.payload.pools,
         loading: false,
       };
-    case actions.DELETE_EMPLOYEE:
+    case actions.DELETE_POOL:
       return {
         ...state,
-        employees: state.employees.filter(
-          (employee) => employee._id !== action.payload.id
+        pools: state.pools.filter((pool) => pool._id !== action.payload.id),
+      };
+    case actions.ADD_POOL:
+      return {
+        ...state,
+        pools: [action.payload.pool, ...state.pools],
+      };
+    case actions.UPDATE_POOL:
+      return {
+        ...state,
+        pools: state.pools.map((pool) =>
+          pool._id === action.payload.pool._id ? action.payload.pool : pool
         ),
       };
-    case actions.ADD_EMPLOYEE:
-      return {
-        ...state,
-        employees: [action.payload.employee, ...state.employees],
-      };
-    case actions.UPDATE_EMPLOYEE:
-      return {
-        ...state,
-        employees: state.employees.map((employee) =>
-          employee._id === action.payload.employee._id
-            ? action.payload.employee
-            : employee
-        ),
-      };
-    case actions.EMPLOYEES_LOADING:
+    case actions.POOLS_LOADING:
       return {
         ...state,
         loading: true,
