25a49b60e1d5eeee48ce9a17d7b1d251bfb7ace6
NESMeki <test@Mikes-MacBook-Pro.local>
2020-11-23 22:25:49 -0500

    Displays pool maps

--- client/src/components/PoolMapping.js
+++ client/src/components/PoolMapping.js
@@ -1,7 +1,65 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Container, ListGroup, ListGroupItem, Button } from "reactstrap";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { connect } from "react-redux";
+import { getPoolMaps, deletePoolMap } from "../actions/poolMapActions";
+import PropTypes from "prop-types";
 
-const PoolMapping = () => {
-    return <div><p>Pain.</p></div>
+const PoolMapping = (props) => {
+    const { poolMaps } = props.poolMap;
+
+    useEffect(() => {
+      props.getPoolMaps();
+    }, []);
+  
+    let onDeleteClick = (id) => {
+      props.deletePoolMap(id);
+    };
+
+    return (
+        <Container>
+          <ListGroup>
+            <TransitionGroup className="poolMap-list">
+              {poolMaps.map(
+                ({
+                  _id,
+                  testBarcode,
+                  poolBarcode,
+                }) => (
+                    <CSSTransition key={_id} timeout={500} classNames="fade">
+                    <ListGroupItem>
+                      &nbsp;
+                      <Button
+                        className="remove-btn"
+                        color="danger"
+                        size="sm"
+                        onClick={onDeleteClick.bind(this, _id)}
+                      >
+                        &times;
+                      </Button>
+                      Pool: {poolBarcode}    Test: {testBarcode}
+                    </ListGroupItem>
+                  </CSSTransition>
+                )
+              )}
+            </TransitionGroup>
+          </ListGroup>
+        </Container>
+      );
 };
 
-export default PoolMapping;
+//export default PoolMapping;
+
+PoolMapping.propTypes = {
+    getPoolMaps: PropTypes.func.isRequired,
+    deletePoolMAp: PropTypes.func.isRequired,
+    poolMap: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+    poolMap: state.poolMap,
+});
+
+export default connect(mapStateToProps, { getPoolMaps, deletePoolMap })(
+    PoolMapping
+);
