da09300dc03cabbd69cf49c5d382fe9573c15f20
Zhen Yi Pan <zharnite@gmail.com>
2020-11-26 14:32:09 -0500

    changed state capitalization

--- client/src/components/WellTesting.js
+++ client/src/components/WellTesting.js
@@ -2,72 +2,77 @@ import React, { useEffect } from "react";
 import { Container, ListGroup, ListGroupItem, Button } from "reactstrap";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import { connect } from "react-redux";
-import { getWellTestings, deleteWellTesting } from "../actions/wellTestingActions";
+import {
+  getWellTestings,
+  deleteWellTesting,
+} from "../actions/wellTestingActions";
 import PropTypes from "prop-types";
 import WellTestingAddModal from "./WellTestingAddModal";
 import WellTestingEditModal from "./WellTestingEditModal";
 
 const WellTesting = (props) => {
-    const { wellTestings } = props.wellTesting;
+  const { wellTestings } = props.wellTesting;
 
-    useEffect(() => {
-      props.getWellTestings();
-    }, []);
-  
-    let onDeleteClick = (id) => {
-      props.deleteWellTesting(id);
-    };
+  useEffect(() => {
+    props.getWellTestings();
+  }, []);
 
-    return (
-        <Container>
-          <WellTestingAddModal />
-          <ListGroup>
-            <TransitionGroup className="wellTesting-list">
-              {wellTestings.map(
-                ({
-                  result,
-                  _id,
-                  poolBarcode,
-                  wellBarcode,
-                  testingStartTime,
-                  testingEndTime,
-                }) => (
-                    <CSSTransition key={_id} timeout={500} classNames="fade">
-                    <ListGroupItem>
-                      <Button
-                        className="remove-btn"
-                        color="danger"
-                        size="sm"
-                        onClick={onDeleteClick.bind(this, _id)}
-                      >
-                        &times;
-                      </Button> <br /> 
-                      Result: {result} <br />
-                      _id: {_id} <br />
-                      poolBarcode: {poolBarcode} <br />
-                      wellBarcode: {wellBarcode} <br /> 
-                      testingStartTime: {testingStartTime} <br />   
-                      testingEndTime: {testingEndTime} <br />                 
-                    </ListGroupItem>
-                  </CSSTransition>
-                )
-              )}
-            </TransitionGroup>
-          </ListGroup>
-        </Container>
-      );
+  let onDeleteClick = (id) => {
+    props.deleteWellTesting(id);
+  };
+
+  return (
+    <Container>
+      <WellTestingAddModal />
+      <ListGroup>
+        <TransitionGroup className="wellTesting-list">
+          {wellTestings.map(
+            ({
+              result,
+              _id,
+              poolBarcode,
+              wellBarcode,
+              testingStartTime,
+              testingEndTime,
+            }) => (
+              <CSSTransition key={_id} timeout={500} classNames="fade">
+                <ListGroupItem>
+                  <WellTestingEditModal />
+                  <Button
+                    className="remove-btn"
+                    color="danger"
+                    size="sm"
+                    onClick={onDeleteClick.bind(this, _id)}
+                  >
+                    &times;
+                  </Button>{" "}
+                  <br />
+                  Result: {result} <br />
+                  _id: {_id} <br />
+                  poolBarcode: {poolBarcode} <br />
+                  wellBarcode: {wellBarcode} <br />
+                  testingStartTime: {testingStartTime} <br />
+                  testingEndTime: {testingEndTime} <br />
+                </ListGroupItem>
+              </CSSTransition>
+            )
+          )}
+        </TransitionGroup>
+      </ListGroup>
+    </Container>
+  );
 };
 
 WellTesting.propTypes = {
-    getWellTestings: PropTypes.func.isRequired,
-    deleteWellTesting: PropTypes.func.isRequired,
-    wellTesting: PropTypes.object.isRequired,
+  getWellTestings: PropTypes.func.isRequired,
+  deleteWellTesting: PropTypes.func.isRequired,
+  wellTesting: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = (state) => ({
-    wellTesting: state.wellTesting
+  wellTesting: state.wellTesting,
 });
 
 export default connect(mapStateToProps, { getWellTestings, deleteWellTesting })(
-    WellTesting
+  WellTesting
 );
--- client/src/components/WellTestingEditModal.js
+++ client/src/components/WellTestingEditModal.js
@@ -15,107 +15,108 @@ import { updateWellTesting } from "../actions/wellTestingActions";
 import PropTypes from "prop-types";
 
 const WellTestingEditModal = (props) => {
-    console.log(props);
+  console.log(props);
 
-    const wellTesting = props.WellTesting.wellTestings.filter( //this part is problematic 
-        (wellTesting) => wellTesting._id === props.id
-    )[0];
+  const wellTesting = props.wellTesting.wellTestings.filter(
+    //this part is problematic
+    (wellTesting) => wellTesting._id === props.id
+  )[0];
 
-    const [modal, setModal] = useState(false);
-    const [result, setResult] = useState("");
-    const [poolBarcode, setPoolBarcode] = useState(0);
-    const [wellBarcode, setWellBarcode] = useState(0);
-    const toggle = () => setModal(!modal);
+  const [modal, setModal] = useState(false);
+  const [result, setResult] = useState("");
+  const [poolBarcode, setPoolBarcode] = useState(0);
+  const [wellBarcode, setWellBarcode] = useState(0);
+  const toggle = () => setModal(!modal);
 
-    let onChange = (e) => {
-        let change = eval(["set" + e.target.name][0]);
-        change(e.target.value);
+  let onChange = (e) => {
+    let change = eval(["set" + e.target.name][0]);
+    change(e.target.value);
+  };
+
+  let reset = () => {
+    let change = eval([setResult][0]);
+    change("in progress");
+    change = eval([setPoolBarcode][0]);
+    change(-1);
+    change = eval([setWellBarcode][0]);
+    change(-1);
+  };
+
+  let onSubmit = (e) => {
+    e.preventDefault();
+
+    const newWellTest = {
+      result: result,
+      _id: wellTesting._id,
+      poolBarcode: poolBarcode,
+      wellBarcode: wellBarcode,
     };
 
-    let reset = () => {
-        let change = eval([setResult][0]);
-        change("in progress");
-        change = eval([setPoolBarcode][0]);
-        change(-1);
-        change = eval([setWellBarcode][0]);
-        change(-1);
-    };
+    if (newWellTest.result == "") {
+      newWellTest.result = "in progress";
+    }
 
-    let onSubmit = (e) => {
-        e.preventDefault();
+    if (newWellTest.poolBarcode > -1 && newWellTest.wellBarcode > -1) {
+      props.addWellTesting(newWellTest);
+    }
+    props.updateWellTesting(newWellTest);
+    reset();
+    toggle();
+  };
 
-        const newWellTest = {
-            result: result,
-            _id: wellTesting._id,
-            poolBarcode: poolBarcode,
-            wellBarcode: wellBarcode,
-        };
+  return (
+    <div>
+      <Button color="dark" style={{ marginBottom: "2rem" }} onClick={toggle}>
+        Add Well Testing
+      </Button>
 
-        if (newWellTest.result == "") {
-            newWellTest.result = "in progress";
-        }
-      
-        if (newWellTest.poolBarcode > -1 && newWellTest.wellBarcode > -1) {
-            props.addWellTesting(newWellTest);
-        }
-        props.updateWellTesting(newWellTest);
-        reset();
-        toggle();
-    };
-    
-    return (
-        <div>
-          <Button color="dark" style={{ marginBottom: "2rem" }} onClick={toggle}>
-            Add Well Testing
-          </Button>
-    
-          <Modal isOpen={modal} toggle={toggle}>
-            <ModalHeader toggle={toggle}>Add a Well Testing</ModalHeader>
-            <ModalBody>
-              <Form onSubmit={onSubmit}>
-                <FormGroup>
-                  <Label for="result">Result</Label>
-                  <Input
-                    type="select"
-                    name="Result"
-                    id="result"
-                    onChange={onChange}
-                  >
-                    <option value="in progress">In progress</option>
-                    <option value="negative">Negative</option>
-                    <option value="postive">Positive</option>
-                  </Input>
-                  <Label for="poolBarcode">Pool Barcode</Label>
-                  <Input
-                    type="text"
-                    name="PoolBarcode"
-                    id="poolBarcode"
-                    placeholder="Pool Barcode"
-                    onChange={onChange}
-                  />
-                  <Label for="wellBarcode">Well Barcode</Label>
-                  <Input
-                    type="text"
-                    name="WellBarcode"
-                    id="wellBarcode"
-                    placeholder="Well Barcode"
-                    onChange={onChange}
-                  />
-                  <Button color="dark" style={{ marginTop: "2rem" }} block>
-                    Add Well Testing
-                  </Button>
-                </FormGroup>
-              </Form>
-            </ModalBody>
-          </Modal>
-        </div>
-      );
-}
+      <Modal isOpen={modal} toggle={toggle}>
+        <ModalHeader toggle={toggle}>Add a Well Testing</ModalHeader>
+        <ModalBody>
+          <Form onSubmit={onSubmit}>
+            <FormGroup>
+              <Label for="result">Result</Label>
+              <Input
+                type="select"
+                name="Result"
+                id="result"
+                onChange={onChange}
+              >
+                <option value="in progress">In progress</option>
+                <option value="negative">Negative</option>
+                <option value="postive">Positive</option>
+              </Input>
+              <Label for="poolBarcode">Pool Barcode</Label>
+              <Input
+                type="text"
+                name="PoolBarcode"
+                id="poolBarcode"
+                placeholder="Pool Barcode"
+                onChange={onChange}
+              />
+              <Label for="wellBarcode">Well Barcode</Label>
+              <Input
+                type="text"
+                name="WellBarcode"
+                id="wellBarcode"
+                placeholder="Well Barcode"
+                onChange={onChange}
+              />
+              <Button color="dark" style={{ marginTop: "2rem" }} block>
+                Add Well Testing
+              </Button>
+            </FormGroup>
+          </Form>
+        </ModalBody>
+      </Modal>
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => ({
-    WellTesting: state.WellTesting,
-  });
+  wellTesting: state.wellTesting,
+});
 
 export default connect(mapStateToProps, { updateWellTesting })(
-    WellTestingEditModal
-  );
+  WellTestingEditModal
+);
