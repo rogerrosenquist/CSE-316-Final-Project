ca57554a0a13111e7b4b660cd5e037e5ec3c8cd4
Zhen Yi Pan <zharnite@gmail.com>
2020-11-26 21:56:08 -0500

    added button onclicks LabHome

--- client/src/components/LabHome.js
+++ client/src/components/LabHome.js
@@ -1,14 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import { useHistory, Redirect, withRouter } from "react-router-dom";
-import {
-  Container, Button,
-} from 'reactstrap';
+import { Container, Button } from "reactstrap";
 import { connect } from "react-redux";
 import { getEmployeeTests } from "../actions/employeeTestActions";
 import PropTypes from "prop-types";
 
 const LabHome = (props) => {
-
   const history = useHistory();
   console.log(props);
 
@@ -16,17 +13,36 @@ const LabHome = (props) => {
     props.getEmployeeTests();
   }, []);
 
+  let changePaths = (e, path) => {
+    e.preventDefault();
+    history.push(path);
+  };
+
   if (!props.location.state) {
     console.log(props);
     return <Redirect to="/labtech" />;
   }
-    return (
-      <Container className="LabHome">
-            <Button color="primary" size="lg" block>Pool Mapping</Button>
-            <Button color="primary" size="lg" block>Well Testing</Button>
-      </Container>
-    );
-  }
+  return (
+    <Container className="LabHome">
+      <Button
+        color="primary"
+        size="lg"
+        block
+        onClick={(e) => changePaths(e, "poolmapping")}
+      >
+        Pool Mapping
+      </Button>
+      <Button
+        color="primary"
+        size="lg"
+        block
+        onClick={(e) => changePaths(e, "welltesting")}
+      >
+        Well Testing
+      </Button>
+    </Container>
+  );
+};
 
 const mapStateToProps = (state) => ({
   employeeTest: state.employeeTest,
